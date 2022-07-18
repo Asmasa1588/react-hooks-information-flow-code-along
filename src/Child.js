b4a72cbc21e4f218b223ff20bd36902d18099f21
Asma <as1588ma@gmail.com>
2022-07-17 22:07:00 -0400

    comeplete work

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, childColor }) {
+  return (
+    <div
+      className="child"
+      onClick={onChangeColor}
+      style={{ backgroundColor: childColor }}
+    />
+  );
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -5,13 +5,26 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState("#FFF");
 
+  function handleChangeColor() {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); // update color state to a new value
+  }
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child
+        onChangeColor={() => {
+          handleChangeColor();
+          setChildrenColor(getRandomColor());
+        }}
+        childColor={childrenColor}
+      />
+      <Child onChangeColor={handleChangeColor} childColor={childrenColor} />
     </div>
   );
 }
 
 export default Parent;
+
+// line:15,16 . we are passing down handledowncolor as a Prop to the child component. and whenever we pass a prop, we destructure it where it was uses
